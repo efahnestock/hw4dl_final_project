9992b073de7bfaac0f4ca9bef5f2a9d45c90fbf8
Jillian Ross <jillianr@mit.edu>
2023-04-13 11:00:18 -0400

    Toy separated network

---
+++ models/separated_network.py
@@ -0,0 +1,16 @@
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class ToyNet(nn.Module):
+    def __init__(self):
+        super(ToyNet, self).__init__()
+        self.fc1 = nn.Linear(8, 10)
+        self.fc2 = nn.Linear(10, 10)
+        self.fc3 = nn.Linear(10, 2)
+
+    def forward(self, x):
+        z = F.relu(self.hid1(x))
+        z = F.relu(self.hid2(z))
+        z = self.fc3(z)  # No activation
+        return z
