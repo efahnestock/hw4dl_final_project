335d47770d27a7675401f0fb3d8725048aa93f6f
Shobhita Sundaram <shobhita@30-10-29.wireless.csail.mit.edu>
2023-04-27 10:33:41 -0400

    change kernel size

--- hw4dl/models/shared_cnn.py
+++ hw4dl/models/shared_cnn.py
@@ -3,7 +3,7 @@ import torch.nn as nn
 import pdb
 
 class VariableCNNBackbone(nn.Module):
-    def __init__(self, layer_shapes: tuple, split_idx: int, num_heads: int):
+    def __init__(self, layer_shapes: tuple, split_idx: int, num_heads: int, kernel_size: int=3):
         """
         Create a NN with variable amount of shared backbone
 
@@ -26,7 +26,7 @@ class VariableCNNBackbone(nn.Module):
                 shared_backbone.append(nn.MaxPool2d(kernel_size=2, stride=2))
             else:
                 shared_backbone.append(
-                    nn.Conv2d(c, layer_shapes[i], kernel_size=3, stride=1, padding=1))
+                    nn.Conv2d(c, layer_shapes[i], kernel_size=kernel_size, stride=1, padding=1))
                 if i != len(self.layer_shapes) - 2:
                     shared_backbone.append(nn.ReLU())
                 c = layer_shapes[i]
@@ -42,7 +42,7 @@ class VariableCNNBackbone(nn.Module):
                     head_i.append(nn.MaxPool2d(kernel_size=2, stride=2))
                 else:
                     head_i.append(
-                        nn.Conv2d(c, layer_shapes[j], kernel_size=3, stride=1, padding=1))
+                        nn.Conv2d(c, layer_shapes[j], kernel_size=kernel_size, stride=1, padding=1))
                     if j != len(self.layer_shapes) - 2:
                         head_i.append(nn.ReLU())
                     c = layer_shapes[j]
